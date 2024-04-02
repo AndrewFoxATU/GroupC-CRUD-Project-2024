b273611c21690e917749e7c7fc6fc1229a34e9c8
Andrew Fox <118258647+AndrewFoxATU@users.noreply.github.com>
2024-04-02 14:13:09 +0100

    Create TestConnection.java

---
+++ src/main/java/org/example/TestConnection.java
@@ -0,0 +1,28 @@
+package ie.atu.jdbc.standard;
+import java.sql.*;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try {
+            // Load the driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to the database, replace "exampledatabase" with your database name
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SubwayDatabase", "root", "password");
+            System.out.println("Connection made to connection pool");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the connection when finished
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
